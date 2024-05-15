448023b11ac7aae955dc10070dea8f1e982f297d
tkuenz20 <tkuenz20@my.chemeketa.edu>
2024-05-14 20:45:49 -0700

    Added initial ejs files

--- server/routes/instrument.js
+++ server/routes/instrument.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 // Get all instruments
-router.get('/', (req, res) => {
+router.get('/instruments', (req, res) => {
   res.send('GET all instruments');
 });
 
