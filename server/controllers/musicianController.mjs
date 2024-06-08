c3394375a1c65e9fe6ce742b913e6f98507ab1d4
seaislandbreezy <sophiayfliu@gmail.com>
2024-06-08 12:37:40 -0700

    musician update

--- server/controllers/musicianController.mjs
+++ server/controllers/musicianController.mjs
@@ -1,55 +1,46 @@
-import * as routeHelper from '../routes/routeHelpers.mjs';
-
-//Use express-validator to remove harmful content
-//import { default as validator } from 'express-validator';
-
 import { default as mongoose } from "mongoose";
 import { default as Instrument } from '../models/instrument.mjs';
 import { default as Genre } from '../models/genre.mjs';
 import { default as Musician } from '../models/musician.mjs';
 import { default as Song } from '../models/song.mjs';
 
+// Function to ensure the URL starts with http or https
+function ensureHttp(url) {
+    if (url && !url.startsWith('http://') && !url.startsWith('https://')) {
+        return 'http://' + url;
+    }
+    return url;
+}
+
 async function musicianList(req, res, next) {
     try {
-        //console.log('musicianList called');
         let musicians = await Musician.find().exec();
-
-        res.render('musicians.ejs', {
-            title: 'musicians',
-            musicians: musicians,
-        });
+        res.render('musicians.ejs', { title: 'Musicians', musicians: musicians });
     } catch (err) {
-        //console.error('Error in musicianList:', err);
         next(err);
     }
 }
 
 async function musicianById(req, res, next) {
     try {
-        //console.log('musicianById called with id:', req.params.id);
         const musicianId = req.params.id;
         let musician = await Musician.findById(musicianId).exec();
         if (musician) {
-            let songs = await musician.songs;
-            res.render('singlemusician.ejs', { musician: musician, songs: songs});
-        }
-        else
+            let songs = await Song.find().exec(); // Fetch all songs
+            res.render('singleMusician.ejs', { musician: musician, songs: songs });
+        } else {
             next();
+        }
     } catch (err) {
-        //console.error('Error in musicianById:', err);
         next(err);
     }
 }
 
 async function createMusician(req, res, next) {
     try {
-        //console.log('createMusician called with data:', req.body);
         let musician = new Musician({});
-
-        res.render("musicianForm.ejs", {
-            musician: musician,
-            creatingNew: {new: true}
-        });
+        let songs = await Song.find().exec(); // Fetch all songs
+        res.render("musicianForm.ejs", { musician: musician, songs: songs, creatingNew: { new: true } });
     } catch (err) {
         next(err);
     }
@@ -57,85 +48,56 @@ async function createMusician(req, res, next) {
 
 async function update_get(req, res, next) {
     try {
-      let musician = await Musician.findById(req.params.id).exec();
-  
-      res.render("musicianForm.ejs", {
-        title: `Update ${musician.name}`,
-        musician: musician,
-        creatingNew: {new: false}
-      });
+        let musician = await Musician.findById(req.params.id).exec();
+        let songs = await Song.find().exec(); // Fetch all songs
+        res.render("musicianForm.ejs", { title: `Update ${musician.name}`, musician: musician, songs: songs, creatingNew: { new: false } });
     } catch (err) {
-      next(err);
+        next(err);
     }
-};
-
+}
 
 async function update_post(req, res, next) {
     try {
-        //console.log('updateMusician called with id:', req.params.id, 'and data:', req.body);
         let musician = await Musician.findById(req.params.id).exec();
-        
         if (musician === null) {
-            //console.log('musician not found');
-            musician = new Musician({
-                _id: req.body.id
-            });
+            musician = new Musician({ _id: req.body.id });
         }
-
         musician.name = req.body.name;
-        musician.imageUri = req.body.imageUri;
+        musician.imageUri = ensureHttp(req.body.imageUri);
         musician.anecdote = req.body.anecdote;
-        musician.process = req.body.process;
-        
-        musician
-            .save()
-            .then((musician) => {
-                res.redirect(musician.url);
-            })
-            .catch((err) => {
-                console.log(err.message);
-                res.render("musicianForm.ejs", {
-                    title: `Update ${musician.name}`,
-                    musician: musician,
-                    errors: routeHelper.errorParser(err.message),
-                });
+        musician.processVideoUri = ensureHttp(req.body.processVideoUri); // Update processVideoUri
+        musician.songs = req.body.songs.split(','); // Update the selected songs
+        musician.save()
+            .then(() => res.redirect(musician.url))
+            .catch(async (err) => {
+                let songs = await Song.find().exec(); // Fetch all songs again
+                res.render("musicianForm.ejs", { title: `Update ${musician.name}`, musician: musician, songs: songs, errors: routeHelper.errorParser(err.message) });
             });
     } catch (err) {
-        console.error('Error in updateMusician:', err);
         next(err);
     }
 }
 
 async function deleteMusician(req, res, next) {
     try {
-        console.log('deletemusician called with id:', req.params.id);
-        
         const musicianId = req.params.id;
         await Instrument.updateMany({ musician: musicianId }, { $unset: { musician: 1 } }).exec();
         await Song.updateMany({ musician: musicianId }, { $unset: { musician: 1 } }).exec();
         await Musician.findByIdAndDelete(musicianId).exec();
         res.redirect('/musician');
     } catch (err) {
-        console.error('Error in deletemusician:', err);
         next(err);
     }
 }
 
 async function verifyDelete(req, res, next) {
     try {
-        //console.log('musicianList called');
         const musicianId = req.params.id;
         let musician = await Musician.findById(musicianId).exec();
-
-        res.render('verifyDeleteForm', {
-            title: 'verifyMusicianDelete',
-            object: musician,
-            objectType: { type: 'musician' }
-        });
+        res.render('verifyDeleteForm', { title: 'Verify Musician Delete', object: musician, objectType: { type: 'musician' } });
     } catch (err) {
-        //console.error('Error in musicianList:', err);
         next(err);
     }
 }
 
-export {musicianList, musicianById, createMusician, deleteMusician, update_get, update_post, verifyDelete}
+export { musicianList, musicianById, createMusician, deleteMusician, update_get, update_post, verifyDelete };
--- server/models/musician.mjs
+++ server/models/musician.mjs
@@ -1,22 +1,22 @@
 import { default as mongoose } from "mongoose";
+import { default as Song } from "./song.mjs";
+
 const Schema = mongoose.Schema;
 
 let MusicianSchema = new Schema({
     name: { type: String, required: true },
     imageUri: { type: String },
     anecdote: { type: String },
-    process: { type: String }
+    processVideoUri: { type: String },
+    songs: [{ type: Schema.Types.ObjectId, ref: 'Song' }]
 });
 
 MusicianSchema.virtual("url").get(function () {
     return "/musician/" + this._id + "/";
 });
 
-import { default as Song } from "./song.mjs";
-
-MusicianSchema.virtual("songs").get(async function () {
-    let songArray = await Song.find().where("musician").equals(this._id).exec();
-    return songArray;
+MusicianSchema.virtual("relatedSongs").get(async function () {
+    return await Song.find({ musician: this._id }).exec();
 });
 
 MusicianSchema.set('toJSON', { virtuals: true });
